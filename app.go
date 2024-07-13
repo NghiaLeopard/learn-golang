e57ace14d89aa0743014bf7936cd6305079064f7
DaiNghiadz~ <nghiabeo1605gmail.com0>
2024-07-13 09:45:06 +0700

    first commit

---
+++ app.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	const inflation = 10
+	var test float64 = 10
+
+	var test1 float64
+
+	fmt.Print("Test 1: ")
+	fmt.Scan(&test1)
+
+	fmt.Println(test)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module example.com/first-init
+
+go 1.22.5
